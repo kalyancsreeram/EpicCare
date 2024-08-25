6f13efeec2e67ffc712e8d183c61a2b3b14419e7
rumanstheddy <sumanthcob@gmail.com>
2024-08-25 14:09:27 -0400

    updated image dimensions in service page to be uniform

--- src/components/servicescomp/MediaControlDiv.js
+++ src/components/servicescomp/MediaControlDiv.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import Typography from '@mui/material/Typography';
-import './styles.scss';
+import React from "react";
+import Typography from "@mui/material/Typography";
+import "./styles.scss";
 
 function MediaControlDiv(props) {
-  let styleString = props.styleLR === 'Left'?  'mediaDivContainer mediaDivLeft' : 'mediaDivContainer mediaDivRight';
+  let styleString =
+    props.styleLR === "Left"
+      ? "mediaDivContainer mediaDivLeft"
+      : "mediaDivContainer mediaDivRight";
   return (
-    <div className={styleString} >
-        <img alt='SeizureCare' src={props.source}/>
-        <div className='mediaDivTextContainer'>
-            <Typography className='service-title' component="div" variant="h6">
-                {props.title}
-            </Typography>
-            <Typography variant="p" color="text.secondary" component="div" className='services_text'>
-               {props.desc}
-            </Typography>
-        </div>
+    <div className={styleString}>
+      <img alt="Seizure Care Service" src={props.source} />
+      <div className="mediaDivTextContainer">
+        <Typography className="service-title" component="div" variant="h6">
+          {props.title}
+        </Typography>
+        <Typography
+          variant="p"
+          color="text.secondary"
+          component="div"
+          className="services_text"
+        >
+          {props.desc}
+        </Typography>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MediaControlDiv
+export default MediaControlDiv;
