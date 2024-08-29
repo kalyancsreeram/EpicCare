996a8dec4fb32a20396cc06da9440f2870ce30be
rumanstheddy <sumanthcob@gmail.com>
2024-08-29 09:17:15 -0400

    webpack config and other changes for font consistency

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,64 @@
-const path = require( 'path' );
+const path = require("path");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
-
 module.exports = {
-		entry: './src/index.js',
+  entry: "./src/index.js",
 
-		output: {
-            filename: 'bundle.js',
-			path: path.resolve( __dirname, './dist' ),
-		    publicPath: ''
-		},
-		mode: 'development',	
-		// devServer: {
-		// 	static: {
-		// 	  directory: path.join(__dirname, './dist'),
-		// 	},		
-		// 	compress: true,
-		// 	port: 9000,
-		//   },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "",
+  },
+  mode: "development",
+  // devServer: {
+  // 	static: {
+  // 	  directory: path.join(__dirname, './dist'),
+  // 	},
+  // 	compress: true,
+  // 	port: 9000,
+  //   },
 
-		module: {
-			rules: [
-				{
-					test: /\.js|\.jsx$/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ["@babel/preset-react", "@babel/preset-env"],
-						}
-					},
-					exclude: /node_modules/
-				},				
-				{
-					test: /\.(png|jpg|gif|JPG|jpeg)$/,
-					type: 'asset/resource'
-				},
-                {
-					test: /\.css$/,
-					use: [				
-						  MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'					
-					]
-				},
-                {
-					test: /\.scss$/,
-					use: [
-						MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'					
-					]
-				},
-			],
-		},
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif|JPG|jpeg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 
-		plugins: [
-			new MiniCssExtractPlugin({
-				filename: 'styles.css',
-				// chunkFilename: '[id].css'
-			}),
-			new HtmlWebpackPlugin({
-				template: "./public/index.html",
-				favicon: "./public/favicon.ico"
-			}),
-		],
-
-	};
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      // chunkFilename: '[id].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+};
