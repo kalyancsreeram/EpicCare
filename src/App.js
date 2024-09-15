da0d2d755533a7aa939771a5fc99e95ce590a6e8
rumanstheddy <sumanthcob@gmail.com>
2024-09-15 17:22:14 -0400

    added whatsapp widget

--- src/App.js
+++ src/App.js
@@ -46,10 +46,67 @@ const theme = createTheme({
   },
 });
 
+function useWhatsappScript(url, options) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = url;
+
+    script.onload = () => {
+      if (window.CreateWhatsappChatWidget) {
+        window.CreateWhatsappChatWidget(options);
+      } else console.error("CreateWhatsappChatWidget is not defined.");
+    };
+
+    script.onerror = () => {
+      console.error("Script loading failed");
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [url, options]);
+}
+
 function App() {
   useEffect(() => {
     document.title = "Seizure Care";
   }, []);
+
+  const url =
+    "https://wati-integration-prod-service.clare.ai/v2/watiWidget.js?79565";
+
+  const options = {
+    enabled: true,
+    chatButtonSetting: {
+      backgroundColor: "#25D366",
+      ctaText: "Chat with us",
+      borderRadius: "25",
+      marginLeft: "0",
+      marginRight: "20",
+      marginBottom: "20",
+      ctaIconWATI: false,
+      position: "right",
+    },
+    brandSetting: {
+      brandName: "SeizureCare",
+      brandSubTitle: "undefined",
+      brandImg: "https://www.wati.io/wp-content/uploads/2023/04/Wati-logo.svg",
+      welcomeText: "Hi there!\nHow can I help you?",
+      messageText: "Hello, %0A I have a question about SeizureCare.",
+      backgroundColor: "#25D366",
+      ctaText: "Chat with us",
+      borderRadius: "25",
+      autoShow: false,
+      phoneNumber: "9199598192479",
+    },
+  };
+
+  useWhatsappScript(url, options);
+
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
