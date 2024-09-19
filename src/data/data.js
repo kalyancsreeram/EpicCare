2be4dece9564c3afd7324a7f6dfd162f79eae018
rumanstheddy <sumanthcob@gmail.com>
2024-09-18 22:25:28 -0400

    mobile responsive changes for research page

---
+++ src/data/data.js
@@ -0,0 +1,74 @@
+const reseachNotes = [
+  {
+    title: "Etiology and Pathophysiology",
+    content:
+      "Uncovering the underlying causes, mechanisms, and neural circuits involved in epilepsy.",
+  },
+  {
+    title: "Genomics and Epigenomics",
+    content:
+      "Exploring the genetic and epigenetic factors contributing to epilepsy, including gene expression and regulation.",
+  },
+  {
+    title: "Biomarkers and Diagnosis",
+    content:
+      "Developing novel biomarkers and diagnostic tools for accurate and early detection of epilepsy.",
+  },
+  {
+    title: "Therapeutics and Treatment",
+    content:
+      "Investigating innovative pharmacological, surgical, and alternative approaches to manage and treat epilepsy.",
+  },
+  {
+    title: "Personalized Medicine",
+    content:
+      "Identifying individualized treatment strategies based on patient-specific characteristics, genetics, and brain function.",
+  },
+  {
+    title: "Neurostimulation and Neuromodulation",
+    content:
+      "Examining the efficacy of techniques like deep brain stimulation, transcranial magnetic stimulation, and transcranial direct current stimulation.",
+  },
+  {
+    title: "Cognitive and Behavioral Comorbidities",
+    content:
+      "Understanding the relationships between epilepsy, cognitive impairment, and psychiatric disorders.",
+  },
+  {
+    title: "Epilepsy Surgery and Neuroimaging",
+    content:
+      "Refining surgical techniques and utilizing advanced neuroimaging modalities like MRI, EEG, and magnetoencephalography.",
+  },
+  {
+    title: "Pediatric and Developmental Epilepsy",
+    content:
+      "Investigating the unique aspects of epilepsy in children, including developmental and epileptic encephalopathies.",
+  },
+  {
+    title: "Comorbidities and Co-occurring Conditions",
+    content:
+      "Examining the interplay between epilepsy and other conditions like autism, ADHD, and migraines.",
+  },
+  {
+    title: "Epilepsy in Special Populations",
+    content:
+      "Addressing the distinct challenges and needs of epilepsy care in vulnerable populations, such as older adults, pregnant women, and underserved communities.",
+  },
+  {
+    title: "Basic and Translational Research",
+    content:
+      "Conducting fundamental studies on epilepsy mechanisms and translating findings into clinical applications.",
+  },
+  {
+    title: "Clinical Trials and Outcomes Research",
+    content:
+      "Evaluating the efficacy and safety of novel treatments and assessing patient outcomes.",
+  },
+  {
+    title: "Epilepsy Advocacy and Stigma Reduction",
+    content:
+      "Promoting awareness, education, and support to combat stigma and improve quality of life for individuals with epilepsy.",
+  },
+];
+
+export { reseachNotes };
