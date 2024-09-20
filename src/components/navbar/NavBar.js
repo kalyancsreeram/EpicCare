c17b2f2572c2ce325028900dd87c0c92f36bc351
rumanstheddy <sumanthcob@gmail.com>
2024-09-19 21:24:46 -0400

    made footer responsive for mobile screens

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,86 +1,109 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import "./styles.scss";
-import LoginModal from "../loginmodal/LoginModal";
-import SignupModal from "../signupmodal/SignupModal";
+import "./NavBar.scss";
+// import LoginModal from "../loginmodal/LoginModal";
+// import SignupModal from "../signupmodal/SignupModal";
 import { useAuth } from "../../utils/auth";
 
 //import CompanyLogo from '../../assets/images/CompanyLogoSVG.svg'
-import CompanyLogo from "../../assets/images/CompanyLogoEdited.png";
-import { decodeToken } from "../../utils/tokenUtil";
-import { Button } from "@mui/material";
+import CompanyLogo from "../../assets/images/CompanyLogoEdited-in.png";
+// import { decodeToken } from "../../utils/tokenUtil";
+import { Button, IconButton, Menu } from "@mui/material";
+import { TiThMenu } from "react-icons/ti";
 
 function NavBar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
   const navigate = useNavigate();
-  const auth = useAuth();
+  // const auth = useAuth();
 
-  useEffect(() => {
-    const token = decodeToken();
-    console.log(`Auth --> ${auth}`);
-    auth.login(token);
-  }, []);
+  // useEffect(() => {
+  //   const token = decodeToken();
+  //   console.log(`Auth --> ${auth}`);
+  //   auth.login(token);
+  // }, []);
 
-  const loginBtnHandler = () => {
-    document.getElementById("loginModal").style.display = "block";
-  };
+  // const loginBtnHandler = () => {
+  //   document.getElementById("loginModal").style.display = "block";
+  // };
   const homeBtnHandler = () => {
     navigate("/");
   };
 
-  const logoutBtnHandler = () => {
-    auth.logout();
-    navigate("/");
+  // const logoutBtnHandler = () => {
+  //   auth.logout();
+  //   navigate("/");
+  // };
+
+  // const signupBtnHandler = () => {
+  //   document.getElementById("signupModal").style.display = "block";
+  // };
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
   };
 
-  const signupBtnHandler = () => {
-    document.getElementById("signupModal").style.display = "block";
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const navListItems = [
+    "Home",
+    "Services",
+    "Order",
+    "Research",
+    "Resources",
+    "About",
+    "Contact",
+  ];
+
+  const getNavLinks = () => {
+    return navListItems.map((item) => {
+      const link = item !== "Home" ? `/${item.toLowerCase()}` : `/`;
+      return <NavLink to={link}>{item}</NavLink>;
+    });
   };
 
   return (
     <div>
       <nav>
         <img
-          className="homeBtn"
+          className="home-btn"
           alt="SeizureCare"
           style={{ width: "200px", cursor: "pointer", left: "10" }}
           src={CompanyLogo}
           onClick={homeBtnHandler}
         />
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/services">Services</NavLink>
-        <NavLink to="/order">Order</NavLink>
-        <NavLink to="/research">Research</NavLink>
-        <NavLink to="/resources">Resources</NavLink>
-        <NavLink to="/about">About Us</NavLink>
-        <NavLink to="/contact-us">Contact Us</NavLink>
-        {/* {
-            auth.user && (
-              <NavLink to='/physician-portal'>Physician</NavLink>  
-            )
-          }   
-          {
-            !auth.user && (
-              <button className='loginBtn' onClick={loginBtnHandler}>Physician Login</button> 
-            )
-          }
-          {
-            !auth.user && (
-              <button className='signupBtn' onClick={signupBtnHandler}>Sign Up</button> 
-            )
-          }
-          {
-            auth.user && (
-              <button className='logoutBtn' onClick={logoutBtnHandler}>Logout</button> 
-            )
-          }         */}
-        <div className="loginBtn-container">
-          <Button variant="contained" className="loginBtn" href="/wip">
-            Login
-          </Button>
+        <div className="nav-links-desktop">
+          {getNavLinks()}
+          <div className="login-btn-container">
+            <Button variant="contained" className="login-btn" href="/wip">
+              Login
+            </Button>
+          </div>
+        </div>
+        <div className="nav-links-mobile">
+          <IconButton id="burger-icon" onClick={(e) => handleClick(e)}>
+            <TiThMenu />
+          </IconButton>
+          <Menu
+            id="burgerMenu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+          >
+            {getNavLinks()}
+            <div className="login-btn-container">
+              <Button variant="contained" className="login-btn" href="/wip">
+                Login
+              </Button>
+            </div>
+          </Menu>
         </div>
       </nav>
-      <LoginModal />
-      <SignupModal />
+      {/* <LoginModal />
+      <SignupModal /> */}
     </div>
   );
 }
