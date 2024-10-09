de3e099d0b1230565d02297177ee18d2f206651e
rumanstheddy <sumanthcob@gmail.com>
2024-10-08 20:54:56 -0400

    changed contact form to use MUI

--- src/components/contactform/ContactForm.js
+++ src/components/contactform/ContactForm.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./ContactForm.scss";
-import { countriesList } from "../../utils/countries";
+import { countriesList } from "../../data/countries";
 import axios from "axios";
 import { CONSTANTS } from "../../Constants";
 
 import FadeLoader from "react-spinners/FadeLoader";
+import { Alert, Button, MenuItem, TextField, Typography } from "@mui/material";
 
 const ContactForm = () => {
   const [loading, setLoading] = useState(false);
-  let [color] = useState("#9b2c79");
   const [formData, setFormData] = useState({
     firstname: "",
     lastname: "",
@@ -18,6 +18,11 @@ const ContactForm = () => {
     message: "",
   });
 
+  const [alert, setAlert] = useState({
+    type: "success",
+    shouldShow: false,
+  });
+
   const override = {
     display: "block",
     margin: "0 auto",
@@ -25,19 +30,19 @@ const ContactForm = () => {
   };
 
   const handleChange = (event) => {
+    console.log(event.target.value);
     const { name, value } = event.target;
     setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
 
   const handleSubmit = (event) => {
-    document.getElementById("contactUs").disabled = true;
+    document.getElementById("contactForm").disabled = true;
     window.scrollTo({ top: 0, behavior: "smooth" });
     setLoading(true);
     event.preventDefault();
-    console.log(formData);
     axios
       .post(`${CONSTANTS.serverURL}/contactus`, { ...formData })
-      .then((response) => {
+      .then(() => {
         setFormData({
           firstname: "",
           lastname: "",
@@ -46,31 +51,43 @@ const ContactForm = () => {
           email: "",
           message: "",
         });
-        document.getElementById("contactUs").disabled = false;
+        document.getElementById("contactForm").disabled = false;
         setLoading(false);
-        alert("Data has been successfully sent...");
+
+        const successAlert = {
+          type: "success",
+          shouldShow: true,
+        };
+
+        setAlert(successAlert);
       })
       .catch(() => {
-        document.getElementById("contactUs").disabled = false;
+        document.getElementById("contactForm").disabled = false;
         setLoading(false);
-        alert("Something went wrong. Try again later");
-        // setResult({ success: false, message: 'Something went wrong. Try again later'});
+
+        const errorAlert = {
+          type: "error",
+          shouldShow: true,
+        };
+
+        setAlert(errorAlert);
       });
   };
-  const style = {
+
+  const loaderColor = "#35294d";
+  const loaderStyle = {
     position: "fixed",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
+    zIndex: "999",
   };
-  const { firstname, phone, email } = formData;
-  const enabled =
-    firstname.length > 0 && (phone.length > 0 || email.length > 0);
+
   return (
-    <>
-      <div style={style}>
+    <div className="contact-page-container">
+      <div style={loaderStyle}>
         <FadeLoader
-          color={color}
+          color={loaderColor}
           loading={loading}
           cssOverride={override}
           size={150}
@@ -78,117 +95,135 @@ const ContactForm = () => {
           data-testid="loader"
         />
       </div>
-      <div id="contactUs" className="contactFormContainer">
-        <h1 className="contactFormTitle">Contact Us</h1>
+      <div className="contact-page-info">
+        <Typography
+          className="main-title"
+          gutterBottom
+          variant="h1"
+          component="div"
+        >
+          Contact Us
+        </Typography>
+
+        <p> Thank you for choosing SeizureCare.</p>
         <p>
-          Thank you for your interest in SeizureCare. Please fill out the below
-          form with your comments or message.
+          We value your inquiries and are here to assist you. Please complete
+          the form below, and we will respond to your message as soon as
+          possible.
         </p>
-        <p className="contactNote">
-          NOTE: Please use this form for general information purposes only. DO
-          NOT send personal health information through this form.
-        </p>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <label className="boldText" htmlFor="fname">
-              First Name
-            </label>
-            <input
-              type="text"
-              className="contactFormInputElement"
-              id="firstname"
-              name="firstname"
-              placeholder="First Name"
-              value={formData.firstname}
-              onChange={handleChange}
-            />
-            <label className="boldText" htmlFor="lname">
-              Last Name
-            </label>
-            <input
-              type="text"
-              className="contactFormInputElement"
-              id="lastname"
-              name="lastname"
-              placeholder="Last Name"
-              value={formData.lastname}
-              onChange={handleChange}
-            />
-            <label className="boldText" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="text"
-              className="contactFormInputElement"
-              id="email"
-              name="email"
-              placeholder="Email Address"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <label className="boldText" htmlFor="phone">
-              Phone
-            </label>
-            <input
-              type="number"
-              className="contactFormInputElement"
-              id="phone"
-              name="phone"
-              placeholder="Phone Number"
-              value={formData.phone}
-              onChange={handleChange}
-            />
-            <label className="boldText" htmlFor="country">
-              Country
-            </label>
-            <select
-              className="countryField"
-              name="country"
-              id="country"
-              value={formData.country}
-              onChange={handleChange}
-            >
-              {countriesList.map((country) => (
-                <option value={country.name} key={country.code}>
-                  {country.name}
-                </option>
-              ))}
-            </select>
-
-            <label className="boldText" htmlFor="message">
-              Comments/Message
-            </label>
-            <textarea
-              className="contactTextArea contactFormInputElement"
-              id="message"
-              name="message"
-              placeholder="Type your message here..."
-              value={formData.message}
-              onChange={handleChange}
-            />
-
-            {/* <fieldset className='preferenceSet'>
-                    <legend>Preferred Method of Communication</legend>
-                    <div>
-                    <input type='checkbox' className='preference contactFormInputElement' id='preferencePhone' name='preferencePhone' value='phone'/>
-                    <label htmlFor="preferencePhone">By Phone</label>                  
-                    </div>
-                    <div>
-                    <input type='checkbox' id='preferenceEmail' className='preference contactFormInputElement' name='preferenceEmail' value='email'/>
-                    <label htmlFor="preferenceEmail">By Email</label>                  
-                    </div>
-                  </fieldset>      */}
-            <input
-              className="contactSubmit"
-              type="submit"
-              value="Submit"
-              disabled={!enabled}
-            ></input>
-          </form>
-        </div>
       </div>
-    </>
+      <div className="contact-form-container">
+        {alert.shouldShow && (
+          <Alert
+            severity={alert.type}
+            onClose={() => {
+              setAlert({});
+            }}
+            className="alert-message"
+          >
+            {alert.type === "success"
+              ? `Your form has been submitted successfully! Thank you for your input.`
+              : `There was an error submitting your form. Please check your information and try again.`}
+          </Alert>
+        )}
+        <form onSubmit={handleSubmit} id="contactForm" className="contact-form">
+          <TextField
+            type="text"
+            id="fName"
+            name="firstname"
+            className="form-input"
+            label="First Name"
+            variant="outlined"
+            value={formData.firstname}
+            onChange={handleChange}
+            helperText="Please enter your first name."
+            required
+          />
+          <TextField
+            type="text"
+            id="lName"
+            name="lastname"
+            className="form-input"
+            label="Last Name"
+            variant="outlined"
+            value={formData.lastname}
+            onChange={handleChange}
+            helperText="Please provide your last name."
+            required
+          />
+          <TextField
+            type="email"
+            id="email"
+            name="email"
+            className="form-input"
+            label="Email Address"
+            variant="outlined"
+            value={formData.email}
+            onChange={handleChange}
+            helperText="Please enter a valid email address."
+            required
+          />
+          <TextField
+            type="tel"
+            id="phone"
+            name="phone"
+            className="form-input"
+            label="Phone Number"
+            variant="outlined"
+            value={formData.phone}
+            onChange={handleChange}
+            helperText="Please provide your contact number."
+            required
+          />
+          <TextField
+            select
+            className="form-dropdown"
+            name="country"
+            id="countrySelect"
+            value={formData.country}
+            onChange={handleChange}
+            helperText="Please select your country."
+          >
+            {countriesList.map((country) => (
+              <MenuItem key={country.code} value={country.name}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            id="message"
+            name="message"
+            className="form-input form-text-area"
+            label="Type your comments here"
+            multiline
+            rows={4}
+            helperText="Please feel free to leave any comments or feedback."
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+          <Button
+            variant="contained"
+            className="form-submit-button"
+            type="submit"
+            id="formSubmitBtn"
+            size="large"
+          >
+            Submit
+          </Button>
+        </form>
+      </div>
+      <div className="contact-page-note">
+        <p>Please use this form for general inquiries only.</p>
+        <p>
+          <b>DO NOT</b> submit any personal health information through this
+          form.
+        </p>
+      </div>
+    </div>
   );
 };
+
+//TODO: Add Spinner and notification after
 
 export default ContactForm;
