37121b7305c5e8ecd4de3776d06c3b15e4587b92
Sumanth Reddy <sumanthcob@gmail.com>
2024-11-30 19:58:17 -0500

    added coa in home

--- src/components/dashboardpage/welcomeComp/Welcome.js
+++ src/components/dashboardpage/welcomeComp/Welcome.js
@@ -5,9 +5,12 @@ import Typography from "@mui/material/Typography";
 import WelcomeImg from "../../../assets/images/welcomeImg.jpg";
 
 import { homePageData } from "../../../data/data";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 function Welcome() {
   const { companyInfo } = homePageData;
+  const navigate = useNavigate();
 
   //TODO: Clean the unused code
   return (
@@ -43,6 +46,30 @@ function Welcome() {
           <p className="home-text--default">{companyInfo.intro}</p>
           <p className="home-text--default">{companyInfo.serviceCommitment}</p>
           <p className="home-text--default">{companyInfo.futureVision}</p>
+
+          <div className="cta-section">
+            <Typography
+              gutterBottom
+              variant="h4"
+              component="div"
+              className="cta-title"
+            >
+              Order Services Easily
+            </Typography>
+            <p className="welcome__cta-description">
+              Need help with EEG, EMU, or Epilepsy Care? Fill out our service
+              order form, and our team will reach out to confirm your request.
+              Experience seamless support tailored to your needs.
+            </p>
+            <Button
+              variant="contained"
+              size="large"
+              className="cta-button"
+              onClick={() => navigate("/order")}
+            >
+              Order
+            </Button>
+          </div>
         </div>
       </div>
     </div>
